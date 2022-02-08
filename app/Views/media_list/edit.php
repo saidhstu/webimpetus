c9eb352e721f562a1083af5b1b6e4add1d06db01
ariful_fb <ariful.fb@gmail.com>
2022-02-08 21:52:19 +0600

    add blocks.

--- app/Views/media_list/edit.php
+++ app/Views/media_list/edit.php
@@ -42,3 +42,12 @@
 
 
 <?php require_once (APPPATH.'Views/common/footer.php'); ?>
+<!-- main content part end -->
+
+<script>
+    // Add the following code if you want the name of the file appear on select
+    $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
+</script>
