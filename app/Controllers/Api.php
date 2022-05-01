c80b95bb220dd9fb89bdc120e84f8a4d017c963f
helal <helal@boo2.nl>
2022-05-01 15:57:10 +0600

    Header added for CORS

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -23,6 +23,9 @@ class Api extends BaseController
 	  $this->gmodel = new Gallery_model();
 	  $this->sec_model = new Secret_model();
 	  header('Content-Type: application/json; charset=utf-8');
+	  header('Access-Control-Allow-Origin: *');
+	  header('Access-Control-Allow-Methods: GET, POST, PUT,DELETE,OPTIONS');
+	  header('Access-Control-Allow-Headers: Accept,Authorization,Content-Type');
 	}
 	
     public function index()
