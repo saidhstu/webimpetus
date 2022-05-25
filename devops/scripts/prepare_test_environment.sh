5986fa48552fcd45537d00e8a5affea2ca0912a0
Balinder Walia <balinder.walia@gmail.com>
2022-05-25 11:19:25 +0200

    EKS moved to K3S

--- devops/scripts/prepare_test_environment.sh
+++ devops/scripts/prepare_test_environment.sh
@@ -4,6 +4,9 @@ set -x
 
 #aws eks
 
+rm -Rf env
+cp /home/bwalia/env_webimpetus_dev_ci4baseimagetest env
+
 export KUBECONFIG=/home/bwalia/.kube/k3s-test.yml
 #aws eks update-kubeconfig --name $KUBENETES_CLUSTER_NAME --region $AWS_DEFAULT_REGION
 
