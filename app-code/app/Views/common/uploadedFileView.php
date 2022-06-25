b1a56c905c8e5aa8567d96a433d92b2b4bfa6cdd
Balinder Walia <balinder.walia@gmail.com>
2022-06-25 11:55:17 +0200

    Cleared app code

--- app-code/app/Views/common/uploadedFileView.php
+++
@@ -1,5 +0,0 @@
-
-
-<input type="hidden" value="<?php echo $file_path;?>" name="file">
-<img class="img-rounded" src="<?php echo $file_path;?>" width="100px">
-<a href="" id="delete_file" class="btn btn-danger"><i class="fa fa-trash"></i></a>
