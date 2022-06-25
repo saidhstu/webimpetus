6ebd3522c40de539f5c3b47bf662277ecb79bd9e
Balinder Walia <balinder.walia@gmail.com>
2022-06-25 12:23:32 +0200

    Cleared app code

--- devops/scripts/deploy_test_environment.sh
+++ devops/scripts/deploy_test_environment.sh
@@ -2,15 +2,15 @@
 
 set -x
 
-scp -r app/ bwalia@192.168.0.69:/home/bwalia/projects/webimpetus/app/
+scp -r app/* bwalia@192.168.0.69:/home/bwalia/projects/webimpetus/app/
 
-/bin/bash prepare_test_environment.sh
+#/bin/bash prepare_test_environment.sh
 
 #kubectl delete -f /var/www/html/writable/tizohub_deployments/service-$SERVICE_ID.yaml
 
-aws sts get-caller-identity
-export KUBECONFIG=/home/bwalia/.kube/k3s-test.yml
+# aws sts get-caller-identity
+# export KUBECONFIG=/home/bwalia/.kube/k3s-test.yml
 
-helm upgrade -f devops/webimpetus-chart/values-test.yaml -i ci4baseimagetest ./devops/webimpetus-chart --set=image.tag=$IMAGE_TAG
+# helm upgrade -f devops/webimpetus-chart/values-test.yaml -i ci4baseimagetest ./devops/webimpetus-chart --set=image.tag=$IMAGE_TAG
 
-kubectl get all -A
+# kubectl get all -A
