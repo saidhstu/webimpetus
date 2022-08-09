018050b03390012724a65a502ae852f373f39d9e
Balinder Walia <balinder.walia@gmail.com>
2022-08-09 17:38:42 +0200

    Pipeline testing

--- build_and_deploy_test_env.sh
+++ build_and_deploy_test_env.sh
@@ -8,3 +8,9 @@ docker-compose down
 docker-compose build
 docker-compose up -d
 docker-compose ps
+
+sleep 30
+
+docker cp /home/bwalia/env_webimpetus_dev_ci4baseimagetest lamp-php74:/var/www/html/.env
+docker exec -it lamp-php74 chown -R www-data:www-data /var/www/html/writable/cache/
+docker exec -it lamp-php74 chown -R www-data:www-data /var/www/html/writable/session/
