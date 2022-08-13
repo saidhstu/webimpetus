7b1608cb599f106dbd02b97794b3ea5615003302
Balinder Walia <balinder.walia@gmail.com>
2022-08-13 17:16:43 +0200

    Reset containers bash script updated to clean workspace folder first

---
+++ prepare_workspace_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x
+sudo -S rm -Rf /home/bwalia/actions-runner-webimpetus/_work/webimpetus/webimpetus/data
+sudo -S rm -Rf /home/bwalia/actions-runner-webimpetus/_work/webimpetus/webimpetus/config
+sudo -S rm -Rf /home/bwalia/actions-runner-webimpetus/_work/webimpetus/webimpetus/ci4/writable/
+sudo -S rm -Rf /home/bwalia/actions-runner-webimpetus/_work/webimpetus/webimpetus/data/
