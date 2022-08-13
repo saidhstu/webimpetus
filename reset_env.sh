290722dd58b07a60a910c42688bfd248c68c4e9f
Balinder Walia <balinder.walia@gmail.com>
2022-08-13 19:30:19 +0200

    Reset env remove .git folder

--- reset_env.sh
+++ reset_env.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # This deploys CI4 project (mariadb, php_lamp, phpmyadmin) in docker container to test environment using docker compose.
 
-#set -x
-
-#rm -Rf ci4/
-
 set -e
 EXIT_CODE=0
-/bin/bash /home/bwalia/prepare_workspace_env.sh || EXIT_CODE=$?
+#/bin/bash /home/bwalia/prepare_workspace_env.sh || EXIT_CODE=$?
+rm -Rf .git || EXIT_CODE=$?
 echo $EXIT_CODE
