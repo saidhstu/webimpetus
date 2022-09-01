8fbbe7931edf2bdd182a4f548fdce327f5a80930
Balinder Walia <balinder.walia@gmail.com>
2022-09-01 19:44:44 +0200

    Pipeline dev env made better more variablised code coming next few days

--- reset_containers.sh
+++ reset_containers.sh
@@ -11,28 +11,38 @@ else
    target_env=$1
 fi
 
-sleep 10
+target_env_short=$target_env
 
+sleep 5
+
+DATE_GEN_VERSION=$(date +"%Y%m%d%I%M%S")
+export DATE_GEN_VERSION=$(date +"%Y%m%d%I%M%S")
+
+mkdir /tmp
+touch /tmp/.env
+#PROD
 if [[ "$target_env" == "production" ]]; then
-
-docker cp /home/bwalia/env_webimpetus_myworkstation prod-workstation-php74:/var/www/html/.env
-docker exec prod-workstation-php74 chown -R www-data:www-data /var/www/html/writable/
-docker exec prod-workstation-php74 composer update
+target_env_short="prod"
+echo APP_DEPLOYED_AT=$DATE_GEN_VERSION >> /home/bwalia/env_webimpetus_myworkstation
+docker cp /home/bwalia/env_webimpetus_myworkstation ${target_env_short}-workstation-php74:/var/www/html/.env
 
 else
-
-
+# DEV
 if [[ "$target_env" == "development" ]]; then
-
-docker cp ${HOME}/env_webimpetus_myworkstation dev-workstation-php74:/var/www/html/.env
-docker exec dev-workstation-php74 chown -R www-data:www-data /var/www/html/writable/
-docker exec dev-workstation-php74 composer update
-
+target_env_short="dev"
+cp ${HOME}/env_webimpetus_myworkstation /tmp/.env
+echo APP_DEPLOYED_AT=$DATE_GEN_VERSION >> /tmp/.env
+docker cp /tmp/.env dev-workstation-php74:/var/www/html/.env
 
 else
-docker cp /home/bwalia/env_webimpetus_dev_ci4baseimagetest test-workstation-php74:/var/www/html/.env
-docker exec test-workstation-php74 chown -R www-data:www-data /var/www/html/writable/
-docker exec test-workstation-php74 composer update
+# TEST
+cp /home/bwalia/env_webimpetus_dev_ci4baseimagetest /tmp/.env
+echo APP_DEPLOYED_AT=$DATE_GEN_VERSION >> /tmp/.env
+docker cp /tmp/.env test-workstation-php74:/var/www/html/.env
 
 fi
 fi
+
+docker exec ${target_env_short}-workstation-php74 chown -R www-data:www-data /var/www/html/writable/
+docker exec ${target_env_short}-workstation-php74 composer update
+
