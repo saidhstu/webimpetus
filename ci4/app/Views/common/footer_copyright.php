52d44a5b25494d74d58eb1b7c01db9691adc5b70
Balinder Walia <balinder.walia@gmail.com>
2022-09-04 01:03:06 +0200

    Version number and footer copyright updated

--- ci4/app/Views/common/footer_copyright.php
+++ ci4/app/Views/common/footer_copyright.php
@@ -18,13 +18,13 @@
                     $appEnvironment = getenv('APP_ENVIRONMENT') ?: "Production";
 
                     if ($appEnvironment == "Production") {
-                        $webImpetusCopyRight = "© " . auto_copyright() . " Webimpetus. All rights reserved.";
+                        $webImpetusCopyRight = "© " . auto_copyright() . " All rights reserved.";
                     } else {
-                        $webImpetusCopyRight = "© " . auto_copyright() . " Webimpetus. All rights reserved. " . $appEnvironment . " Environment.";
+                        $webImpetusCopyRight = "© " . auto_copyright() . " All rights reserved. " . ucfirst($appEnvironment) . " Environment.";
                     }
 
                     ?>
-                    <p><?php auto_copyright("2009"); ?>&nbsp;&copy;&nbsp;Workstation&nbsp;-&nbsp;Powered&nbsp;by&nbsp;<a href="https://webimpetus.cloud/"> <i class="ti-heart"></i><?php echo $webImpetusCopyRight; ?></a></p>
+                    <p><?php auto_copyright("2009"); ?>&nbsp;&copy;&nbsp;Workstation&nbsp;-&nbsp;Powered&nbsp;by&nbsp;<a href="https://webimpetus.cloud/"> <i class="ti-heart"></i>&nbsp;Webimpetus</a>&nbsp;<?php echo $webImpetusCopyRight; ?></p>
                     <p><a target="_blank" href="<?php echo $appReleaseNotesDocURL; ?>"> WebImpetus <?php echo getenv('APP_DEPLOYED_AT'); ?></a>
                     </p>
                 </div>
