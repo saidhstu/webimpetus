98b6b0a2b215011dee3f3fb10d84f01b3a39033c
Balinder Walia <balinder.walia@gmail.com>
2022-10-23 14:06:52 +0200

    Bwalia dev env added for easier local Rancher k3s development

--- build_and_deploy_env_to_k3s.sh
+++ build_and_deploy_env_to_k3s.sh
@@ -6,7 +6,8 @@
 #   set -x
 
 SVC_HOST=localhost
-SVC_NODEPORT=32180
+SVC_NODEPORT=32080
+
 DATE_GEN_VERSION=$(date +"%Y%m%d%I%M%S")
 
 TARGET_CLUSTER="k3s-rancher-desktop"
@@ -35,7 +36,16 @@ else
    k3s_deployment_tool=$3
 fi
 
-if [[ "$targetEnv" == "dev" || "$targetEnv" == "test" || "$targetEnv" == "prod" ]]; then
+if [[ -z "$4" ]]; then
+   echo "docker base image is empty, so setting docker base image to dev-wsl_webserver (default)"
+   docker_base_image="${targetEnv}-wsl_webserver"
+else
+   echo "docker base image type is NOT empty, docker base image is set to $4"
+   docker_base_image=$4
+fi
+
+
+if [[ "$targetEnv" == "dev" || "$targetEnv" == "dev-bwalia" || "$targetEnv" == "test" || "$targetEnv" == "prod" ]]; then
 echo "The targetEnv is $targetEnv supported by this script"
 else
 echo "Oops! The targetEnv is $targetEnv is not supported by this script, check the README.md and try again! (Hint: Try default value is dev)"
@@ -60,7 +70,7 @@ fi
 export APP_RELEASE_NOTES_DOC_URL=$APP_RELEASE_NOTES_DOC_URL
 
 ##### Set some variables
-if [[ "$targetEnv" == "dev" ]]; then
+if [[ "$targetEnv" == "dev" || "$targetEnv" == "dev-bwalia" ]]; then
 WORKSPACE_DIR=$(pwd)
 fi
 
@@ -102,7 +112,7 @@ if [[ "$cicd_action" == "start" ]]; then
 # this builds the image name 
 #docker rmi -f $(docker images -aq)
 docker-compose -f "${WORKSPACE_DIR}/docker-compose.yml" build                #up -d --build
-docker tag ${targetEnv}-wsl_webserver registry.workstation.co.uk/webimpetus:${DATE_GEN_VERSION}
+docker tag ${docker_base_image} registry.workstation.co.uk/webimpetus:${DATE_GEN_VERSION}
 docker push registry.workstation.co.uk/webimpetus:${DATE_GEN_VERSION}
 
 #docker build -f devops/kubernetes/Dockerfile -t registry.workstation.co.uk/workstation:latest .
