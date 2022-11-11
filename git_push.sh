87981d966fafb9c8f51b277d571010303d2f70e6
Balinder Walia <balinder.walia@gmail.com>
2022-11-11 20:43:39 +0100

    Updated bwalia devops  enhancements

--- git_push.sh
+++ git_push.sh
@@ -5,10 +5,12 @@
 
 set -x
 
+git checkout devops/prod_deployment_setup
+
 git add devops
-git add ci4
-git commit -m "Updated bwalia ${date} features"
+#git add ci4
+git commit -m "Updated bwalia devops ${date} enhancements"
 #git push
-git push --set-upstream origin devops/enhanced-pipeline
+git push --set-upstream origin devops/prod_deployment_setup
 
 
